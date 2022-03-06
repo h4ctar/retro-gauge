6ae850664005af2d02f7402703edad20f2635d22
Ben Smith <bensmith87@gmail.com>
2022-03-06 14:48:38 +1100

    resolves #36: better long press

--- code/button.c
+++ code/button.c
@@ -14,6 +14,8 @@ uint8_t lastState = BUTTON_UP;
 uint8_t currentState;
 uint32_t pressedTime = 0;
 uint32_t releasedTime = 0;
+uint8_t isPressing = 0;
+uint8_t isLongDetected = 0;
 
 uint8_t shortPress = 0;
 uint8_t longPress = 0;
@@ -27,7 +29,10 @@ void updateButton() {
 
     if (lastState == BUTTON_UP && currentState == BUTTON_DOWN) {
         pressedTime = millis();
+        isPressing = 1;
+        isLongDetected = 0;
     } else if (lastState == BUTTON_DOWN && currentState == BUTTON_UP) {
+        isPressing = 0;
         releasedTime = millis();
 
         uint32_t pressDuration = releasedTime - pressedTime;
@@ -35,7 +40,14 @@ void updateButton() {
         if (pressDuration < SHORT_PRESS_DURATION) {
             shortPress = BUTTON_DOWN;
             longPress = BUTTON_UP;
-        } else {
+        }
+    }
+
+    if (isPressing == 1 && isLongDetected == 0) {
+        long pressDuration = millis() - pressedTime;
+
+        if (pressDuration > SHORT_PRESS_DURATION) {
+            isLongDetected = 1;
             shortPress = BUTTON_UP;
             longPress = BUTTON_DOWN;
         }
